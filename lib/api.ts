55123aa3bb32f1843db440ed76cb1f55c7e2e6c8
Dmytro Basamyhin <dima17lit@gmail.com>
2025-08-24 12:39:25 +0300

    fix

--- lib/api.ts
+++ lib/api.ts
@@ -2,16 +2,16 @@ import axios from "axios";
 import { type Note, type NewNote } from "@/types/note"
 
 
-const myKey = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
-
 interface NotesHttpResponse {
     notes: Note[],
     totalPages: number
 }
 
-// if (!myKey) {
-//   throw new Error("NEXT_PUBLIC_NOTEHUB_TOKEN is not defined in the environment variables.");
-// }
+function ensureKey() {
+  const myKey = process.env.NEXT_PUBLIC_NOTEHUB_TOKEN;
+  if (!myKey) throw new Error("Auth token is missing. Please set NEXT_PUBLIC_NOTEHUB_TOKEN.");
+  return myKey;
+}
 
 const url = `https://notehub-public.goit.study/api/notes`;
 
@@ -24,7 +24,7 @@ export async function fetchNotes(page: number, perPage: number, searchTerm: stri
             search: searchTerm,
     }, 
     headers: {
-        Authorization: `Bearer ${myKey}`,
+        Authorization: `Bearer ${ensureKey()}`,
         Accept: 'application/json'
         }
     }) 
@@ -35,7 +35,7 @@ export async function fetchNotes(page: number, perPage: number, searchTerm: stri
 export async function deleteNote(id: string) {
     const res = await axios.delete<Note>(`${url}/${id}`, {
     headers: {
-        Authorization: `Bearer ${myKey}`,
+        Authorization: `Bearer ${ensureKey()}`,
         Accept: 'application/json'
         }
     })
@@ -46,7 +46,7 @@ export async function deleteNote(id: string) {
 export async function createNote(newNote: NewNote) {
     const res = await axios.post<Note>(`${url}`, newNote, {
     headers: {
-        Authorization: `Bearer ${myKey}`,
+        Authorization: `Bearer ${ensureKey()}`,
         Accept: 'application/json'
         }
     })
@@ -57,7 +57,7 @@ export async function createNote(newNote: NewNote) {
 export async function fetchNotesById(id: string){
     const res = await axios.get<Note>(`${url}/${id}`, {
     headers: {
-        Authorization: `Bearer ${myKey}`,
+        Authorization: `Bearer ${ensureKey()}`,
         Accept: 'application/json'
         }
     })
